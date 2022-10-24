1910b13cb5de0d1fccde3e43e4a073bd32e8fdd8
oksanaivashko <oksivashko@gmail.com>
2022-10-24 13:27:58 -0500

    added create_ec2.py

---
+++ create_ec2.py
@@ -0,0 +1,11 @@
+# Create EC2 Instance
+import boto3
+ec2 = boto3.resource('ec2')
+
+instances = ec2.create_instances(
+        ImageId="ami-09d3b3274b6c5d4aa",
+        MinCount=1,
+        MaxCount=1,
+        InstanceType="t2.micro",
+        KeyName="WindowsKey"
+    )
