05b94c3e17e7954c3cfbaed04b29c5e0949828f5
oksanaivashko <oksivashko@gmail.com>
2022-10-31 11:58:57 -0500

    added practice_task.py

---
+++ practice_task.py
@@ -0,0 +1,5 @@
+print("Welcome to Python!")
+name = input("What is your name? ")
+place = input("What is your favorite place to visit? ")
+hobbi = input("What is your hobbi? ")
+print(f"Nice to meet you {name} :)")
